91f2604275244862a3b5098d23dddb63bcccb42d
Stanley Lim <stannielim@meta.com>
2025-01-30 19:53:34 -0500

    authentication register and logout

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const { PrismaClient } = require("@prisma/client");
 
@@ -12,6 +14,42 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    const token = jwt.sign(newUser, "LUNA");
+
+    res.send(token);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// if you make a get request to /account
+// with the `token`, that means you can get my user data
+app.get("/account", async (req, res, next) => {
+  // 1. take in the token
+  const token = req.headers.authorization;
+
+  // 2. decode the token to figure out who this token belongs to (who is the logged in user)
+  // what is a token?
+  // a string that is "encrypted". and it can be "decrypted" to get the user
+  const user = jwt.decode(token, "LUNA");
+
+  // 3. send the user data
+  res.send(user);
 });
 
 app.listen(3000);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,111 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+  return <h1>Login</h1>;
+};
+
+const Register = ({ setUser }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const register = async (event) => {
+    event.preventDefault();
+
+    // data is the token
+    const { data } = await axios.post("http://localhost:3000/register", {
+      username,
+      password,
+    });
+
+    window.localStorage.setItem("token", data);
+
+    const response = await axios.get("http://localhost:3000/account", {
+      headers: {
+        authorization: data,
+      },
+    });
+
+    setUser(response.data);
+    navigate("/account");
+  };
+
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={register}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button>Register</button>
+      </form>
+    </>
+  );
+};
+
+const Account = () => {
+  return <h1>Account</h1>;
+};
+
 function App() {
-  return <></>;
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+
+    const tryToLogin = async () => {
+      if (token) {
+        const response = await axios.get("http://localhost:3000/account", {
+          headers: {
+            authorization: token,
+          },
+        });
+
+        setUser(response.data);
+        navigate("/account");
+      }
+    };
+
+    tryToLogin();
+  }, []);
+
+  const logout = () => {
+    setUser(null);
+    window.localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <>
+      {user ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <div>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      )}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register setUser={setUser} />} />
+        <Route path="/account" element={<Account user={user} />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
